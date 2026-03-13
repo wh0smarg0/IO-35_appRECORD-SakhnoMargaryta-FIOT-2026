cf747f8d4612f84ee64127e8fc2b8fe8e507cbb5
wh0smarg0 <wh0smarg0.s@gmail.com>
2026-03-13 10:58:21 +0200

    change base-path

--- base-path.js
+++ base-path.js
@@ -1,3 +1,3 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/Labs-Markdown-Template" : "");
+  (process.env.NODE_ENV === "production" ? "/IO-35_appRECORD-SakhnoMargaryta-FIOT-2026" : "");
